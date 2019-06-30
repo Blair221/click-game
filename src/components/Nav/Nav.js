1c92844b4c8c9cd902cbab46dd8641c985954606
Blair Brownlie <bbrownli@asu.edu>
2019-06-29 21:31:55 -0700

    initial file setup I thought I had already done

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Nav extends React.Component {
+  state = {};
+  render() {
+    return (
+      <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          Click-Game
+        </a>
+      </nav>
+    );
+  }
+}
+
+export default Nav;
